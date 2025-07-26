cfa839b3c66a18cffca11a7ade03908ac6508983
FrontEndAngular <jlchz19@gmai.com>
2025-07-26 17:29:49 -0400

    fix: actualizar backend con endpoints de verificación de email y limpieza de base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  version: '1.0.1'
+  apiUrl: 'https://prueba1-5jnd.onrender.com',
+  version: '1.0.3',
+  timestamp: Date.now()
 }; 
